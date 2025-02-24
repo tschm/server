b9285fcd2f4df476e0e6270660bc23da155dba6a
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-02-24 12:39:40 +0400

    Detailed README

--- template/src/{{ project_name }}/server.py
+++ template/src/{{ project_name }}/server.py
@@ -2,11 +2,11 @@ import numpy as np
 from tschm.flight import Server
 
 
-class BallServer(Server):
+class TestServer(Server):
     def f(self, matrices: dict[str, np.ndarray]) -> dict[str, np.ndarray]:
         self.logger.info(f"Matrices: {matrices.keys()}")
         return {key : 2*value for key, value in matrices.items()}
 
 
 if __name__ == "__main__":  # pragma: no cover
-    BallServer.start()
+    TestServer.start()
